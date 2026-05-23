7f176b62c0c542eaf75de1236dd5d7af9a582020
Divyanshu951 <divyanshuk9515@gmail.com>
2026-05-23 15:03:37 +0530

    remove reviews button

--- src/components/ui/SideNavBar.tsx
+++ src/components/ui/SideNavBar.tsx
@@ -1,4 +1,4 @@
-import { Link, MessageCircle, Play } from "lucide-react";
+import { Link, Play } from "lucide-react";
 
 type Props = {
   id: string;
@@ -8,10 +8,10 @@ type Props = {
 export default function SideNavBar({ id, title }: Props) {
   return (
     <div className="fixed top-[50%] right-4 z-1001 flex translate-y-[-50%] flex-col items-center gap-1 rounded-md bg-black p-1">
-      <a className="flex h-10 w-10 cursor-pointer flex-col items-center justify-between rounded-[3px] bg-gray-700 px-2 py-1 text-gray-300 transition-colors duration-300 hover:text-gray-100">
+      {/* <a className="flex h-10 w-10 cursor-pointer flex-col items-center justify-between rounded-[3px] bg-gray-700 px-2 py-1 text-gray-300 transition-colors duration-300 hover:text-gray-100">
         <MessageCircle size={18} />
         <span className="text-[8px]">Reviews</span>
-      </a>
+      </a> */}
       <a
         href={`https://www.youtube.com/results?search_query=${title}`}
         target="_blank"
